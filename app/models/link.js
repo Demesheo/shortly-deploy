d238598c54d7487af6172042f0cd4808c02f02a4
demesheo <d.wilsonii91@gmail.com>
2015-07-30 19:19:38 -0700

    Refactored link.js

--- app/models/link.js
+++ app/models/link.js
@@ -1,18 +1,29 @@
 var db = require('../config');
 var crypto = require('crypto');
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+Schema = mongoose.Schema;
 
-var LinkSchema= db.Model.extend({
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var LinkSchema = mongoose.Schema({
+  visits : Number,
+  link : String,
+  title : String,
+  code : String,
+  base_url : String,
+  url : String
 });
 
-LinkSchema.on('init', LinkSchema.initialize());
+var Link = mongoose.model('Link', linkSchema);
 
-module.exports = mongoose.model('Link', LinkSchema);
+var createSha = function (url){
+  var shasum = crypto.createHash('sha1');
+  shasum.update(model.get('url'));
+  return shasum.digest('hex').slice(0, 5);
+};
+
+linkSchema.pre('save', function (next){
+  var code = createSha(this.url);
+  this.code = code;
+  next();
+});
+
+module.exports = Link;
